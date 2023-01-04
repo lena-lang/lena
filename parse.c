4c8e8a1d415afcf95fa0b944cc123400d884fbee
ahsouza <annibalhsouza@gmail.com>
2023-01-03 21:42:23 -0300

    arvore de analise a operacoes aritmeticas

--- parse.c
+++ parse.c
@@ -8,6 +8,39 @@
 
 static char buffer[2048];
 
+
+// use operator string to see which operation to perform
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  // if tagged as number return it directly.
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  // the operator is always second child.
+  char* op = t->children[1]->contents;
+
+  // we store the third child in `x`
+  long x = eval(t->children[2]);
+
+  // iterate the remaining children and combining.
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 // fake readline function 
 char* readline(char* prompt) {
   fputs(prompt, stdout);
@@ -28,14 +61,13 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
-
 // new parsers
 mpc_parser_t* Number   = mpc_new("number");
 mpc_parser_t* Operator = mpc_new("operator");
 mpc_parser_t* Expr     = mpc_new("expr");
 mpc_parser_t* Lena    = mpc_new("lena");
 
-/* Define them with the following Language */
+// define them with the following Language
 mpca_lang(MPCA_LANG_DEFAULT,
   "                                                     \
     number   : /-?[0-9]+/ ;                             \
@@ -45,11 +77,10 @@ mpca_lang(MPCA_LANG_DEFAULT,
   ",
   Number, Operator, Expr, Lena);
 
-
 // mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
   puts("Lena Version v0.0.1");
-  puts("Press Ctrl+c to Exit\n");
+  puts("Press Ctrl+C to Exit\n");
 
   while (1) {
     // now in either case readline will be correctly defined
@@ -60,7 +91,10 @@ mpca_lang(MPCA_LANG_DEFAULT,
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lena, &r)) {
       // on success print the AST
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
+      mpc_ast_delete(r.output);
+      // mpc_ast_print(r.output);
       mpc_ast_delete(r.output);
     } else {
       // otherwise print the error
@@ -69,6 +103,5 @@ mpca_lang(MPCA_LANG_DEFAULT,
     }
     free(input);
   }
-
   return 0;
 }
